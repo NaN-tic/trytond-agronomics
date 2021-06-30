ae639420cfbaf3ab48caf062a0d5783d63c8bad0
Àngel Àlvarez <angel@nan-tic.com>
2021-06-30 12:16:37 +0200

    add references on party

--- __init__.py
+++ __init__.py
@@ -2,10 +2,13 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
+from . import party
 from . import plot
+
 
 def register():
     Pool.register(
+        party.Party,
         plot.Enclosure,
         plot.Crop,
         plot.DenominationOrigin,
