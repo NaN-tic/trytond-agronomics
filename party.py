11b70acbf169c6e11bb9e46c4a0bd620b6cd470f
Raimon Esteve <raimonesteve@gmail.com>
2025-01-14 18:59:55 +0100

    Not copy plantations and parcels party

--- party.py
+++ party.py
@@ -3,9 +3,20 @@
 from trytond.model import fields
 from trytond.pool import PoolMeta
 
+
 class Party(metaclass=PoolMeta):
     __name__ = 'party.party'
 
     plantations = fields.One2Many('agronomics.plantation', 'party',
         'Plantation')
     parcels = fields.One2Many('agronomics.parcel', 'producer', 'Parcels')
+
+    @classmethod
+    def copy(cls, parties, default=None):
+        if default is None:
+            default = {}
+        else:
+            default = default.copy()
+        default.setdefault('plantations', None)
+        default.setdefault('parcels', None)
+        return super().copy(parties, default=default)
